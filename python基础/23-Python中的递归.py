bcd901b5cc448001fa83afd619539de8f354ab23
anpeng1013 <anpeng1013@163.com>
2022-08-09 19:56:55 +0800

    add new file

--- python基础/23-Python中的递归.py
+++ python基础/23-Python中的递归.py
@@ -4,4 +4,42 @@
 # @File :23-Python中的递归.py
 # @Software: PyCharm
 
-print('hello world!')
+"""
+    递归的特点：
+        1.函数内部调用自己
+        2.必须要出口。
+"""
+
+
+# 1.累加程序--用递归实现
+def add_num(num):
+    if num == 1:
+        return 1  # 函数出口，如果没有出口会报错：超出最大递归深度：996
+    else:
+        return num + add_num(num - 1)  # 调用自己
+
+
+print(add_num(100))  # 5050
+
+
+# 2.快速排序--用递归实现
+def quick_sort(num_list):
+    if len(num_list) < 2:
+        return num_list
+    else:
+        high = len(num_list) - 1
+        mid = int(high / 2)
+        left = []
+        right = []
+        mid_value = num_list.pop(mid)  # 选了基准元素后，一定要在遍历这数组前删除这个元素，否则
+        # 当第一轮比较大小时，若right数组没有元素，left数组和原数组一样，陷入死循环
+        for i in range(len(num_list)):
+            if num_list[i] > mid_value:
+                right.append(num_list[i])
+            else:
+                left.append(num_list[i])
+        return quick_sort(left) + [mid_value] + quick_sort(right)
+
+
+arg_list = [3, 5, 9, 23, 8, 6]
+print(quick_sort(arg_list))  # [3, 5, 6, 8, 9, 23]
---
+++ python基础/24-Python中的lambda表达式.py
@@ -0,0 +1,11 @@
+# -*- coding =utf-8 -*-
+# @Time : 2022/8/9 19:41
+# @Author : anpeng
+# @File :24-Python中的lambda表达式.py
+# @Software: PyCharm
+
+"""
+    如果一个函数有一个返回值，并且只有一句代码，可以使用lambda简化
+语法：lambda 参数列表：表达式
+
+"""
